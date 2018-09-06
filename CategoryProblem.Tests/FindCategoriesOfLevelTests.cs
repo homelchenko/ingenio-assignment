20968201a115285096861ba612b99f3750e8afcc
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2018-09-06 21:09:45 +0300

    Return empty array when level is empty

--- CategoryProblem.Tests/FindCategoriesOfLevelTests.cs
+++ CategoryProblem.Tests/FindCategoriesOfLevelTests.cs
@@ -34,6 +34,16 @@ namespace CategoryProblem.Tests
             Assert.Equal(new[] {103, 109, 202 }, categories);
         }
 
+        [Fact]
+        public void When4thLevel_ShouldReturnEmpty()
+        {
+            // Act
+            int[] categories = FindCategoriesOfLevel(4);
+
+            // Assert
+            Assert.Equal(new int[0], categories);
+        }
+
         private static int[] FindCategoriesOfLevel(int level)
         {
             return new Solution().FindCategoriesOfLevel(level);
